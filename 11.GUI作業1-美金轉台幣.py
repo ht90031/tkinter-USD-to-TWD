e946d67f8f654828af58b50fd48e9f24adae5ed3
Jamie <109197759+ht90031@users.noreply.github.com>
2022-08-02 11:47:06 +0800

    新增檔案
    新增檔案
    11.GUI作業1-美金轉台幣.py

---
+++ 11.GUI作業1-美金轉台幣.py
@@ -0,0 +1,38 @@
+"""
+======第一題======
+美金轉換後的台幣金額：
+"""
+import tkinter as tk                     # 在Python 3.x 匯入該tkinter 函式庫
+
+win = tk.Tk()                            # 建立GUI 應用程式的主視窗
+win.wm_title("美金轉台幣")                 # 設定主視窗標題
+win.resizable(width=False, height=False) # 設定主視窗不可以被調整大小
+win.minsize(width=360, height=240)       # 最小尺寸
+win.maxsize(width=360, height=240)       # 最大尺寸
+
+total = 0
+def event1():
+    global total
+    # try except會略過錯誤，盡量不要太常做，可能會發現不了程式碼存在的錯誤
+    try:                 # 嘗試處理
+        total = float(entry1Str.get()) * 29.82
+    except:              # 如果失敗
+        total = 0
+    # 另一種寫法，判斷是不是數字
+    # if total.isdigit() == False:
+    #    total = 0
+    total = round(total,2)         # 小數點四捨五入到兩位
+    label2Str.set("轉換後的台幣金額：" + str(total))
+
+
+label1 = tk.Label(win,text="美金:",font=20).place(x=20, y=60)
+
+label2Str = tk.StringVar()
+label2 = tk.Label(win,text="",font=20,textvariable=label2Str).place(x=20, y=120)
+
+entry1Str = tk.StringVar()
+entry1 = tk.Entry(win,textvariable=entry1Str).place(x=80, y=60)
+
+btn1 = tk.Button(win,text="轉換",command=event1).place(x=250, y=60)
+
+win.mainloop()                           # 最後步驟：程式做無限循環
